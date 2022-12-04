808c4eb3724dc14569177e40bf1d1b9235ca753e
Evgenij Belyh <e.belyh@ideco.ru>
2022-12-04 07:14:16 +0300

    Adding symbols

--- docprocess_c/tg_tablegen.c
+++ docprocess_c/tg_tablegen.c
@@ -19,6 +19,10 @@ enum TG_SYMTYPE {
 	TG_SYMTYPE_INPUT
 };
 
+struct tg_variable {
+	struct tg_val *val;
+};
+
 struct tg_function {
 	int argcount;
 };
@@ -30,9 +34,9 @@ struct tg_input {
 
 struct tg_symbol {
 	union {
-		struct tg_val *val;
-		struct tg_function *func;
-		struct tg_input *input;
+		struct tg_variable var;
+		struct tg_function func;
+		struct tg_input input;
 	};
 
 	enum TG_SYMTYPE type;
@@ -42,9 +46,8 @@ struct tg_symbol {
 
 TG_HASHED(struct tg_symbol, TG_HASH_SYMBOL)
 
-struct tg_allocator funcalloc;
-struct tg_allocator inputalloc;
 struct tg_allocator symalloc;
+
 struct tg_hash symtable;
 
 struct tg_val *csv_to_table(int f)
@@ -116,39 +119,14 @@ struct tg_val *readsource(struct tg_input *in,
 	return csv_to_table(p[1]);
 }
 
-void tg_symboladdval(const char *name, void *v, enum TG_SYMTYPE type)
+void tg_symboladd(const char *name, struct tg_symbol *s)
 {
-	struct tg_symbol *sym;
-
-	sym = tg_alloc(&symalloc);
-
-	if (type == TG_SYMTYPE_VARIABLE)
-		sym->val = v;
-	else if (type == TG_SYMTYPE_FUNC)
-		sym->func = v;
-	else if (type == TG_SYMTYPE_INPUT)
-		sym->input = v;
-
-	sym->type = type;
-
-	tg_hashset(TG_HASH_SYMBOL, &symtable, name, sym);
+	tg_hashset(TG_HASH_SYMBOL, &symtable, name, s);
 }
 
 void *tg_symbolget(const char *name, enum TG_SYMTYPE type)
 {
-	struct tg_symbol *sym;
-
-	if ((sym = tg_hashget(TG_HASH_SYMBOL, &symtable, name)) == NULL)
-		return NULL;
-
-	if (type == TG_SYMTYPE_VARIABLE)
-		return sym->val;
-	else if (type == TG_SYMTYPE_VARIABLE)
-		return sym->func;
-	else if (type == TG_SYMTYPE_INPUT)
-		return sym->input;
-	
-	TG_ERROR("Unknown symbol type: %d.", type);
+	return tg_hashget(TG_HASH_SYMBOL, &symtable, name);
 }
 
 struct tg_val *tg_symbolgetval(const char *name)
@@ -159,9 +137,9 @@ struct tg_val *tg_symbolgetval(const char *name)
 		return NULL;
 
 	if (sym->type == TG_SYMTYPE_VARIABLE)
-		return sym->val;
+		return sym->var.val;
 	if (sym->type == TG_SYMTYPE_INPUT)
-		return readsource(sym->input, NULL, 1);
+		return readsource(&(sym->input), NULL, 1);
 	else if (sym->type == TG_SYMTYPE_FUNC) {
 		TG_ERROR("Cannot convert function %s to value.", name);
 		return NULL;
@@ -171,6 +149,8 @@ struct tg_val *tg_symbolgetval(const char *name)
 
 	return NULL;
 }
+
+// symbol call function
 
 void tg_inputdestroy(struct tg_input *in)
 {
@@ -194,7 +174,7 @@ void tg_printsymbols()
 		}
 		if (s->type == TG_SYMTYPE_INPUT) {
 			printf("input{type:\"%s\"; path:\"%s\"}\n",
-				s->input->type, s->input->path);
+				s->input.type, s->input.path);
 		}
 	);
 }
@@ -212,7 +192,7 @@ int tg_readsourceslist(const char *sources)
 	p = strtok(s, ";");
 	do {
 		const char *name, *type, *path;
-		struct tg_input *input;
+		struct tg_symbol *in;
 		char *pp;
 
 		strcpy(ss, p);
@@ -230,11 +210,13 @@ int tg_readsourceslist(const char *sources)
 
 		path = pp;
 
-		input = tg_alloc(&inputalloc);
-		input->type = tg_strdup(type);
-		input->path = tg_strdup(path);
+		in = tg_alloc(&symalloc);
+		in->type = TG_SYMTYPE_INPUT;
+		in->input.type = tg_strdup(type);
+		in->input.path = tg_strdup(path);
 
-		tg_symboladdval(name, input, TG_SYMTYPE_INPUT);
+		tg_symboladd(name, in);
+
 	} while ((p = strtok(NULL, ";")) != NULL);
 
 	free(s);
@@ -245,7 +227,6 @@ int tg_readsourceslist(const char *sources)
 
 int main()
 {
-	tg_allocinit(&inputalloc, sizeof(struct tg_input)); 
 	tg_allocinit(&symalloc, sizeof(struct tg_symbol)); 
 	
 	tg_inithash(TG_HASH_SYMBOL, &symtable);
@@ -262,7 +243,6 @@ int main()
 	tg_endframe();
 
 	tg_destroyhash(TG_HASH_SYMBOL, &symtable);
-	tg_allocdestroy(&inputalloc, (void (*)(void *)) &tg_inputdestroy); 
 	tg_allocdestroy(&symalloc, NULL); 
 
 	return 0;
